e360a502c02622a1d52f613bde038e422cedf61e
mokwathedeveloper <moffatmokwa12@gmail.com>
2025-12-16 12:36:38 +0300

    test: add AgentPayVault test placeholder

---
+++ test/AgentPayVault.test.js
@@ -0,0 +1,14 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("AgentPayVault", function () {
+  // TODO: Add comprehensive tests for AgentPayVault contract
+  
+  it("Should deploy successfully", async function () {
+    // Test deployment
+  });
+  
+  it("Should handle policy-based payments", async function () {
+    // Test payment policies
+  });
+});
